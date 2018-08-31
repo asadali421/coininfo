524a9bc6633ec2cd3169d186fa26dae7e6d3daa6
asad ali <asadali88f@gmail.com>
2018-08-31 17:40:29 +0500

    make it better

--- app.js
+++ app.js
@@ -77,8 +77,11 @@ app.use(cookieParser());
 // view engine setup
 
 
-app.engine('handlebars',exphbs());
+app.engine('handlebars',exphbs({
+    partialsDir: path.join(__dirname, 'views/partials'),
+    layoutsDir: path.join(__dirname, 'views/layouts')}));
 app.set('view engine', 'handlebars');
+app.set('views',path.join(__dirname,'views'));
 
 var port = process.env.PORT || 3000;
 
